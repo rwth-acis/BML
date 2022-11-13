1c2d6cd431d8eebe3af29f5a2c397e11ef1bffd5
Marc Luqué <marc.luque@outlook.com>
2022-11-13 12:34:39 +0100

    Parser: Add graphical output of parse tree

--- src/main/java/BML.java
+++ src/main/java/BML.java
@@ -1,10 +1,13 @@
+import org.antlr.v4.gui.TreeViewer;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+import javax.swing.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Objects;
 
 public class BML {
@@ -21,6 +24,14 @@ public class BML {
         CommonTokenStream tokens = new CommonTokenStream(bmlLexer);
         BMLParser bmlParser = new BMLParser(tokens);
 
-        System.out.println(bmlParser.bot().toStringTree());
+        JFrame frame = new JFrame("BML AST");
+        JPanel panel = new JPanel();
+        TreeViewer viewer = new TreeViewer(Arrays.asList(bmlParser.getRuleNames()), bmlParser.program());
+        viewer.setScale(1.5);
+        panel.add(viewer);
+        frame.add(panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
