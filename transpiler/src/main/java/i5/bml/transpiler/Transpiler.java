74feac4a9663dcea3a3b22ae8b9899a94a41585d
Marc Luqué <marc.luque@outlook.com>
2022-11-26 13:24:16 +0100

    Transpiler: Use appropriate package name

---
+++ transpiler/src/main/java/i5/bml/transpiler/Transpiler.java
@@ -0,0 +1,25 @@
+package i5.bml.transpiler;
+
+import i5.bml.parser.Parser;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+public class Transpiler {
+    public static void main(String[] args) {
+        var fileName = "Example.bml";
+//        var fileName = "OpenAPIPetStoreWithTelegramExample.bml";
+        var inputString = "";
+        try {
+            var inputResource = Objects.requireNonNull(Transpiler.class.getClassLoader().getResource(fileName));
+            inputString = Files.readString(Paths.get(inputResource.toURI()));
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+
+        Parser.parse(inputString);
+    }
+}
