6b3a0512d4d865b8454b962862779d568cad65d6
Marc Luqué <marc.luque@outlook.com>
2022-11-29 12:32:14 +0100

    Transpiler: Add printing of parser errors and diagnostics

--- transpiler/src/main/java/i5/bml/transpiler/Transpiler.java
+++ transpiler/src/main/java/i5/bml/transpiler/Transpiler.java
@@ -1,11 +1,13 @@
 package i5.bml.transpiler;
 
 import i5.bml.parser.Parser;
+import org.eclipse.lsp4j.Diagnostic;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.List;
 import java.util.Objects;
 
 public class Transpiler {
@@ -22,7 +24,15 @@ public class Transpiler {
         }
 
         var stringBuilder = new StringBuilder();
-        var diagnostics = Parser.parse(inputString, stringBuilder);
+        List<Diagnostic> diagnostics = null;
+        try {
+            diagnostics = Parser.parse(inputString, stringBuilder);
+        } catch (Exception e) {
+            System.err.printf("PARSING ERROR: %s%n", e.getMessage());
+        }
+        for (Diagnostic diagnostic : diagnostics) {
+            System.err.println(diagnostic.getMessage());
+        }
         System.out.println(stringBuilder);
     }
 }
