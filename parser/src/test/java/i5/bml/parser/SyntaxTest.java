3eff7c9aa5816f50a7fb866581ca4c205b30b6a8
Marc Luqué <marc.luque@outlook.com>
2022-11-30 16:10:25 +0100

    Parser: Add allowed syntax test

--- parser/src/test/java/i5/bml/parser/SyntaxTest.java
+++ parser/src/test/java/i5/bml/parser/SyntaxTest.java
@@ -1,5 +1,27 @@
 package i5.bml.parser;
 
+import i5.bml.parser.utils.TestUtils;
+import i5.bml.parser.walker.DiagnosticsCollector;
+import org.antlr.v4.runtime.tree.ParseTreeWalker;
+import org.eclipse.lsp4j.Diagnostic;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SyntaxTest {
+
+    // File path relative to test resources folder
+    private static final String ALLOWED_SYNTAX_BML = "AllowedSyntax.bml";
+
+    @Test
+    void checkAllowedSyntax() {
+        var pair = Parser.parse(TestUtils.readFileIntoString(ALLOWED_SYNTAX_BML));
+        var diagnosticsCollector = new DiagnosticsCollector();
+        new ParseTreeWalker().walk(diagnosticsCollector, pair.getRight().program());
+        var diagnostics = diagnosticsCollector.getCollectedDiagnostics().stream()
+                .map(Diagnostic::getMessage)
+                .toList();
+
+        Assertions.assertTrue(diagnostics.isEmpty(), () -> String.join("\n", diagnostics));
+    }
 
 }
