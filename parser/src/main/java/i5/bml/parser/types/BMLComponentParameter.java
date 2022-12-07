8a73e78c7e2314156225ac2e272e880bcd4d4430
Marc Luqué <marc.luque@outlook.com>
2022-12-07 13:32:51 +0100

    Project: Minor cleanup of build files

--- parser/src/main/java/i5/bml/parser/types/BMLComponentParameter.java
+++ parser/src/main/java/i5/bml/parser/types/BMLComponentParameter.java
@@ -1,10 +1,8 @@
 package i5.bml.parser.types;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
+@Documented
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface BMLComponentParameter {
