2afff7aa0708c40bc098ce2725a38108f2cc04f9
Marc Luqué <marc.luque@outlook.com>
2022-12-09 16:15:40 +0100

    Transpiler: Advance code generation of OpenAPI components, still requires finishing a TODO

--- transpiler/src/main/java/i5/bml/transpiler/generators/OpenAPIGenerator.java
+++ transpiler/src/main/java/i5/bml/transpiler/generators/OpenAPIGenerator.java
@@ -1,11 +1,11 @@
 package i5.bml.transpiler.generators;
 
+import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.NullLiteralExpr;
-import com.github.javaparser.ast.expr.StringLiteralExpr;
-import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
+import com.github.javaparser.ast.expr.*;
 import generatedParser.BMLBaseVisitor;
 import generatedParser.BMLParser;
 import i5.bml.parser.types.BMLFunction;
@@ -28,16 +28,28 @@ public class OpenAPIGenerator implements Generator {
 
     @Override
     public Node generateComponent(BMLParser.ComponentContext componentContext, BMLBaseVisitor<Node> visitor) {
-        var block = new BlockStmt();
+        var container = new ClassOrInterfaceDeclaration();
 
+        openAPIComponent.getTagOperationIdPairs().keySet().stream().distinct().forEach(tag -> {
+            var clientClassName = "%sApi".formatted(tag);
+            var type = "ThreadLocal<%s>".formatted(clientClassName);
+            var initializer = new MethodCallExpr(new NameExpr("ThreadLocal"), "withInitial",
+                    new NodeList<>(new MethodReferenceExpr(new NameExpr(clientClassName), new NodeList<>(), "new")));
+            FieldDeclaration field = container.addFieldWithInitializer(type, StringUtils.uncapitalize(clientClassName),
+                    initializer, Modifier.Keyword.PRIVATE, Modifier.Keyword.STATIC, Modifier.Keyword.FINAL);
 
+            // TODO: Try to add import for %sApi. Maybe this method should class that already has a compilationUnit?
 
-        return block;
+            // Add getter & setter
+            container.addMember(field.createGetter());
+        });
+
+        return container;
     }
 
     @Override
     public Node generateFieldAccess(Expression object, TerminalNode field) {
-        throw new IllegalStateException(openAPIComponent.getName() + " does not support function calls");
+        throw new UnsupportedOperationException(openAPIComponent.getName() + " does not support function calls");
     }
 
     @Override
@@ -55,7 +67,7 @@ public class OpenAPIGenerator implements Generator {
         var tagOperationIdPair = openAPIComponent.getTagOperationIdPairs().get(key.toLowerCase());
         var tag = StringUtils.capitalize(tagOperationIdPair.getLeft());
         var operationId = tagOperationIdPair.getRight();
-        String methodCall = "ComponentRegistry.get%sAPI().%s".formatted(tag, operationId);
+        String methodCall = "ComponentRegistry.get%sApi().%s".formatted(tag, operationId);
 
         params.addAll(((BMLFunction) function.type).getOptionalParameters());
         var args = params.stream()
