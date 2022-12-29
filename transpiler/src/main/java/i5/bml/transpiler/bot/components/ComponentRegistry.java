27bead2739009e9356a76f01720b9a00bee35cdc
Marc Luqué <marc.luque@outlook.com>
2022-12-29 12:08:48 +0100

    Transpiler: Use simpler reflections to initialize components

--- transpiler/src/main/java/i5/bml/transpiler/bot/components/ComponentRegistry.java
+++ transpiler/src/main/java/i5/bml/transpiler/bot/components/ComponentRegistry.java
@@ -4,20 +4,21 @@ import i5.bml.transpiler.bot.events.Event;
 import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.PriorityBlockingQueue;
 
 public class ComponentRegistry {
 
     public static void initComponents(ExecutorService threadPool, PriorityBlockingQueue<Event> eventQueue) {
-        Reflections reflections = new Reflections(ComponentRegistry.class.getPackageName());
-        var annotated = reflections.getMethodsAnnotatedWith(ComponentInitializer.class);
-        for (var method : annotated) {
-            try {
-                method.invoke(null, threadPool, eventQueue);
-            } catch (IllegalAccessException | InvocationTargetException e) {
-                throw new RuntimeException(e);
-            }
-        }
+        Arrays.stream(ComponentRegistry.class.getDeclaredMethods())
+                .filter(m -> m.isAnnotationPresent(ComponentInitializer.class))
+                .forEach(m -> {
+                    try {
+                        m.invoke(null, threadPool, eventQueue);
+                    } catch (IllegalAccessException | InvocationTargetException e) {
+                        throw new RuntimeException(e);
+                    }
+                });
     }
 }
