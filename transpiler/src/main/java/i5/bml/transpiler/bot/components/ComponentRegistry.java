4c74ff79983d1736f3c80e30f8609472bf95befc
Marc Luqué <marc.luque@outlook.com>
2022-12-30 18:09:16 +0100

    Transpiler: Start using SLF4J for component registry logging

--- transpiler/src/main/java/i5/bml/transpiler/bot/components/ComponentRegistry.java
+++ transpiler/src/main/java/i5/bml/transpiler/bot/components/ComponentRegistry.java
@@ -1,12 +1,17 @@
 package i5.bml.transpiler.bot.components;
 
 import i5.bml.transpiler.bot.events.Event;
+import i5.bml.transpiler.bot.threads.telegram.TelegramBotThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.concurrent.*;
 
 public class ComponentRegistry {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComponentRegistry.class);
 
     public static void initComponents(ExecutorService threadPool, PriorityBlockingQueue<Event> eventQueue) {
         var futures = Arrays.stream(ComponentRegistry.class.getDeclaredMethods())
@@ -16,17 +21,15 @@ public class ComponentRegistry {
                         m.invoke(null, threadPool, eventQueue);
                     } catch (IllegalAccessException | InvocationTargetException ignore) {}
                 }, threadPool).exceptionally(e -> {
-                    System.err.println(e.getMessage());
-                    e.printStackTrace();
-                    return null;
+                    throw new IllegalStateException(e);
                 }))
                 .toArray(CompletableFuture[]::new);
 
         try {
             CompletableFuture.allOf(futures).get();
-            System.out.println("Component initialization done!");
+            LOGGER.info("Component initialization done!");
         } catch (InterruptedException | ExecutionException e) {
-            throw new IllegalStateException("Component initialization failed", e);
+            LOGGER.error("Component initialization failed", e);
         }
     }
 }
