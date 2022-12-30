e939cd920d037148ee1a1938b0cbd0b690a00ec1
Marc Luqué <marc.luque@outlook.com>
2022-12-30 14:47:02 +0100

    Transpiler: Synchronize bot component initialization by waiting on future completion

--- transpiler/src/main/java/i5/bml/transpiler/bot/components/ComponentRegistry.java
+++ transpiler/src/main/java/i5/bml/transpiler/bot/components/ComponentRegistry.java
@@ -1,24 +1,32 @@
 package i5.bml.transpiler.bot.components;
 
 import i5.bml.transpiler.bot.events.Event;
-import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.*;
 
 public class ComponentRegistry {
 
     public static void initComponents(ExecutorService threadPool, PriorityBlockingQueue<Event> eventQueue) {
-        Arrays.stream(ComponentRegistry.class.getDeclaredMethods())
+        var futures = Arrays.stream(ComponentRegistry.class.getDeclaredMethods())
                 .filter(m -> m.isAnnotationPresent(ComponentInitializer.class))
-                .forEach(m -> {
+                .map(m -> CompletableFuture.runAsync(() -> {
                     try {
                         m.invoke(null, threadPool, eventQueue);
-                    } catch (IllegalAccessException | InvocationTargetException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+                    } catch (IllegalAccessException | InvocationTargetException ignore) {}
+                }, threadPool).exceptionally(e -> {
+                    System.err.println(e.getMessage());
+                    e.printStackTrace();
+                    return null;
+                }))
+                .toArray(CompletableFuture[]::new);
+
+        try {
+            CompletableFuture.allOf(futures).get();
+            System.out.println("Component initialization done!");
+        } catch (InterruptedException | ExecutionException e) {
+            throw new IllegalStateException("Component initialization failed", e);
+        }
     }
 }
