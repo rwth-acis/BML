30fea9b6eff66f12c212a91ac3da3087b9782589
Marc Luqué <marc.luque@outlook.com>
2023-01-05 16:23:44 +0100

    Transpiler: Improve error logging of MessageHelper in bot framework

--- transpiler/src/main/java/i5/bml/transpiler/bot/events/messenger/MessageHelper.java
+++ transpiler/src/main/java/i5/bml/transpiler/bot/events/messenger/MessageHelper.java
@@ -4,7 +4,10 @@ import com.slack.api.methods.SlackApiException;
 import com.slack.api.socket_mode.SocketModeClient;
 import i5.bml.transpiler.bot.events.messenger.slack.SlackUser;
 import i5.bml.transpiler.bot.events.messenger.telegram.TelegramUser;
+import i5.bml.transpiler.bot.threads.slack.SlackBotThread;
 import i5.bml.transpiler.bot.threads.telegram.TelegramComponent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -12,24 +15,20 @@ import java.io.IOException;
 
 public class MessageHelper {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageHelper.class);
+
     public static void replyToMessenger(User user, String msg) {
         if (user instanceof TelegramUser telegramUser) {
             sendTelegramMessage(telegramUser.telegramComponent(), telegramUser.chatId(), msg);
         } else if (user instanceof SlackUser slackUser) {
             sendSlackMessage(slackUser.slackClient(), slackUser.botToken(), slackUser.channelId(), msg);
         } else {
-            // TODO: Throw exception
+            LOGGER.error("Unknown user type {}", user.getClass());
         }
     }
 
     public static void replyToMessenger(MessageEventContext context, String msg) {
-        if (context.event().user() instanceof TelegramUser telegramUser) {
-            sendTelegramMessage(telegramUser.telegramComponent(), telegramUser.chatId(), msg);
-        } else if (context.event().user() instanceof SlackUser slackUser) {
-            sendSlackMessage(slackUser.slackClient(), slackUser.botToken(), slackUser.channelId(), msg);
-        } else {
-            // TODO: Throw exception
-        }
+        replyToMessenger(context.event().user(), msg);
     }
 
     private static void sendTelegramMessage(TelegramComponent telegramComponent, Long chatId, String msg) {
@@ -39,8 +38,8 @@ public class MessageHelper {
             send.setText(msg);
             telegramComponent.execute(send);
         } catch (TelegramApiException e) {
-            // TODO: Throw new exception
-            e.printStackTrace();
+            LOGGER.error("An error occurred while sending the msg '{}' to the chat with id {} using the telegram bot {}:\n{}",
+                    msg, chatId, telegramComponent.getBotUsername(), e.getMessage());
         }
     }
 
@@ -48,8 +47,8 @@ public class MessageHelper {
         try {
             slackClient.getSlack().methods().chatPostMessage(r -> r.token(botToken).channel(channelId).text(msg));
         } catch (IOException | SlackApiException e) {
-            // TODO: Throw new exception
-            e.printStackTrace();
+            LOGGER.error("An error occurred while sending the msg '{}' to the chat with id {} using the slack bot:\n{}",
+                    msg, channelId, e.getMessage());
         }
     }
 }
