401ac43209a8da15ed4eaf408e480ba27a8b3311
Marc Luqué <marc.luque@outlook.com>
2023-01-05 14:13:45 +0100

    Transpiler: Make dialogue stepping more generic by allowing several dialogues

--- transpiler/src/main/java/i5/bml/transpiler/generators/types/components/DialogueGenerator.java
+++ transpiler/src/main/java/i5/bml/transpiler/generators/types/components/DialogueGenerator.java
@@ -2,14 +2,17 @@ package i5.bml.transpiler.generators.types.components;
 
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.expr.MethodCallExpr;
-import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.expr.*;
+import com.github.javaparser.ast.type.UnknownType;
 import generatedParser.BMLParser;
 import i5.bml.parser.types.dialogue.BMLDialogue;
+import i5.bml.transpiler.bot.components.ComponentRegistry;
+import i5.bml.transpiler.bot.dialogue.DialogueAutomaton;
 import i5.bml.transpiler.generators.JavaTreeGenerator;
 import i5.bml.transpiler.generators.CodeGenerator;
 import i5.bml.transpiler.generators.Generator;
+import i5.bml.transpiler.utils.Utils;
 import org.antlr.symtab.Type;
 
 @CodeGenerator(typeClass = BMLDialogue.class)
@@ -21,7 +24,9 @@ public class DialogueGenerator implements Generator {
     public Node generateFunctionCall(Expression object, BMLParser.FunctionCallContext ctx, JavaTreeGenerator visitor) {
         var event = new MethodCallExpr(new NameExpr("ctx"), "event");
         var session = new MethodCallExpr(event, "session");
-        var dialogue = new MethodCallExpr(session, "dialogue");
-        return new MethodCallExpr(dialogue, "step", new NodeList<>(new NameExpr("ctx")));
+        var dialogues = new MethodCallExpr(session, "dialogues");
+        var stepCall = new MethodCallExpr(new NameExpr("d"), "step", new NodeList<>(new NameExpr("ctx")));
+        var lambda = new LambdaExpr(new Parameter(new UnknownType(), "d"), stepCall);
+        return new MethodCallExpr(dialogues, "forEach", new NodeList<>(lambda));
     }
 }
