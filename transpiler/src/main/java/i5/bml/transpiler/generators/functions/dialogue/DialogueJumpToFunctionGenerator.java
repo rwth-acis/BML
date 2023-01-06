60a3f71c658839ea14c9b55a7aab35200661d116
Marc Luqué <marc.luque@outlook.com>
2023-01-06 18:01:52 +0100

    Transpiler: Implement jumpTo function Generator

---
+++ transpiler/src/main/java/i5/bml/transpiler/generators/functions/dialogue/DialogueJumpToFunctionGenerator.java
@@ -0,0 +1,23 @@
+package i5.bml.transpiler.generators.functions.dialogue;
+
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.expr.Expression;
+import com.github.javaparser.ast.expr.MethodCallExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import generatedParser.BMLParser;
+import i5.bml.parser.types.BMLFunctionType;
+import i5.bml.parser.types.functions.dialogue.BMLDialogueJumpToFunction;
+import i5.bml.transpiler.generators.CodeGenerator;
+import i5.bml.transpiler.generators.Generator;
+import i5.bml.transpiler.generators.JavaTreeGenerator;
+
+@CodeGenerator(typeClass = BMLDialogueJumpToFunction.class)
+public class DialogueJumpToFunctionGenerator implements Generator {
+
+    @Override
+    public Node generateFunctionCall(Expression object, BMLParser.FunctionCallContext ctx, JavaTreeGenerator visitor) {
+        var expr = ((BMLFunctionType) ctx.type).getRequiredParameters().get(0).getExprCtx();
+        return new MethodCallExpr(new NameExpr("dialogueAutomaton"), "jumpTo", new NodeList<>((Expression) visitor.visit(expr), new NameExpr("ctx")));
+    }
+}
