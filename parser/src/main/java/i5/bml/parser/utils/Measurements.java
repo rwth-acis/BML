03dea073c3d290b17048b0f53c05f59d80e9436e
Marc Luqué <marc.luque@outlook.com>
2023-01-07 19:47:01 +0100

    Parser: Improve measurement mechanism

--- parser/src/main/java/i5/bml/parser/utils/Measurements.java
+++ parser/src/main/java/i5/bml/parser/utils/Measurements.java
@@ -2,11 +2,16 @@ package i5.bml.parser.utils;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Supplier;
 
 public class Measurements {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Measurements.class);
 
     private Measurements() {
     }
@@ -65,5 +70,21 @@ public class Measurements {
 
         measuredTimes.clear();
         totalTime = 0;
+    }
+
+    public static <T> T measure(String name, Supplier<T> actionToMeasure) {
+        var start = System.currentTimeMillis();
+        T returnValue = actionToMeasure.get();
+        var end = System.currentTimeMillis();
+        LOGGER.info("{} took {} ms", name, (end - start));
+
+        return returnValue;
+    }
+
+    public static void measure(String name, Runnable actionToMeasure) {
+        var start = System.currentTimeMillis();
+        actionToMeasure.run();
+        var end = System.currentTimeMillis();
+        LOGGER.info("{} took {} ms", name, (end - start));
     }
 }
