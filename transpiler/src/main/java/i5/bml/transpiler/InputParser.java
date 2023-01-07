a94b549537faded2b3d221d1835126a508b954ca
Marc Luqué <marc.luque@outlook.com>
2023-01-07 19:47:26 +0100

    Transpiler: Add compilation step when output format is defined to be jar

--- transpiler/src/main/java/i5/bml/transpiler/InputParser.java
+++ transpiler/src/main/java/i5/bml/transpiler/InputParser.java
@@ -1,24 +1,32 @@
 package i5.bml.transpiler;
 
 import i5.bml.parser.Parser;
+import i5.bml.parser.utils.Measurements;
 import i5.bml.parser.walker.DiagnosticsCollector;
 import i5.bml.transpiler.generators.JavaTreeGenerator;
 import i5.bml.transpiler.utils.PrinterUtil;
-import i5.bml.transpiler.utils.Utils;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 import org.apache.commons.cli.*;
 import org.apache.commons.io.FileUtils;
-import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.DiagnosticSeverity;
+import org.gradle.tooling.GradleConnectionException;
+import org.gradle.tooling.GradleConnector;
+import org.gradle.tooling.ProjectConnection;
+import org.gradle.tooling.ResultHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.stringtemplate.v4.ST;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 
 public class InputParser {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(InputParser.class);
 
     private String outputDir;
 
@@ -43,16 +51,16 @@ public class InputParser {
 
         // Start processing input file
         var inputString = FileUtils.readFileToString(new File(inputFilePath), Charset.defaultCharset());
-        var pair = Parser.parse(inputString);
-        ParseTree tree = pair.getRight().program();
+        var pair = Measurements.measure("Parsing", () -> Parser.parse(inputString));
+        var tree = pair.getRight().program();
 
         // Collect diagnostics from parse tree
-        DiagnosticsCollector diagnosticsCollector = new DiagnosticsCollector();
-        ParseTreeWalker.DEFAULT.walk(diagnosticsCollector, tree);
+        var diagnosticsCollector = new DiagnosticsCollector();
+        Measurements.measure("Type checking", () -> ParseTreeWalker.DEFAULT.walk(diagnosticsCollector, tree));
         var diagnostics = diagnosticsCollector.getCollectedDiagnostics();
 
         var containsError = false;
-        for (Diagnostic diagnostic : diagnostics) {
+        for (var diagnostic : diagnostics) {
             System.err.printf("%s line %s: %s%n", diagnostic.getSeverity().name().toUpperCase(),
                     diagnostic.getRange().getStart().getLine(),
                     diagnostic.getMessage());
@@ -62,76 +70,110 @@ public class InputParser {
             }
         }
 
-
         if (!containsError) {
-            // Prepare output directory
-            outputPackage = outputPackage.replaceAll("\\.", "/");
+            invokeCodeGeneration(tree);
+        }
+    }
 
-            // Copies source code to specified directory
-            FileUtils.deleteDirectory(new File(outputDir + "/src/main/java/" + outputPackage));
-            FileUtils.copyDirectory(new File(BOT_DIR), new File(outputDir + "/src/main/java/" + outputPackage));
+    private void invokeCodeGeneration(ParseTree tree) throws IOException {
+        // Prepare output directory
+        outputPackage = outputPackage.replaceAll("\\.", "/");
 
-            for (File file : FileUtils.listFiles(new File(outputDir + "/src/main/java/" + outputPackage), null, true)) {
-                PrinterUtil.readAndWriteJavaFile(file, file.getName().split("\\.")[0], javaFile -> {
-                    //noinspection OptionalGetWithoutIsPresent
-                    var compilationUnit = javaFile.findCompilationUnit().get();
+        // Copies source code to specified directory
+        FileUtils.deleteDirectory(new File(outputDir));
+        FileUtils.copyDirectory(new File(BOT_DIR), new File(outputDir + "/src/main/java/" + outputPackage));
 
-                    // Rename package declaration
-                    //noinspection OptionalGetWithoutIsPresent
-                    var currentPackage = compilationUnit.getPackageDeclaration().get().getName().asString().replaceFirst("i5.bml.transpiler.bot", "");
-                    if (currentPackage.isEmpty()) {
-                        compilationUnit.removePackageDeclaration();
-                    } else {
-                        if (!outputPackage.isEmpty()) {
-                            currentPackage = "%s.%s".formatted(outputPackage, currentPackage);
-                        }
+        for (File file : FileUtils.listFiles(new File(outputDir + "/src/main/java/" + outputPackage), null, true)) {
+            PrinterUtil.readAndWriteJavaFile(file, file.getName().split("\\.")[0], javaFile -> {
+                //noinspection OptionalGetWithoutIsPresent
+                var compilationUnit = javaFile.findCompilationUnit().get();
 
-                        compilationUnit.setPackageDeclaration(currentPackage.replaceFirst("\\.", ""));
+                // Rename package declaration
+                //noinspection OptionalGetWithoutIsPresent
+                var currentPackage = compilationUnit.getPackageDeclaration().get().getName().asString().replaceFirst("i5.bml.transpiler.bot", "");
+                if (currentPackage.isEmpty()) {
+                    compilationUnit.removePackageDeclaration();
+                } else {
+                    if (!outputPackage.isEmpty()) {
+                        currentPackage = "%s.%s".formatted(outputPackage, currentPackage);
                     }
 
-                    // Rename imports
-                    compilationUnit.getImports().stream()
-                            .filter(i -> i.getName().asString().startsWith("i5.bml.transpiler.bot"))
-                            .forEach(i -> {
-                                var importName = i.getName().asString().replaceFirst("i5.bml.transpiler.bot.", "");
-                                if (!outputPackage.isEmpty()) {
-                                    i.setName("%s.%s".formatted(outputPackage, importName));
-                                } else {
-                                    i.setName(importName);
-                                }
-                            });
-                });
-            }
+                    compilationUnit.setPackageDeclaration(currentPackage.replaceFirst("\\.", ""));
+                }
 
-            // Create "project" folders
-            new File(outputDir + "/src/main/resources/").mkdir();
-            new File(outputDir + "/src/test/java/").mkdir();
-            new File(outputDir + "/src/test/resources/").mkdir();
+                // Rename imports
+                compilationUnit.getImports().stream()
+                        .filter(i -> i.getName().asString().startsWith("i5.bml.transpiler.bot"))
+                        .forEach(i -> {
+                            var importName = i.getName().asString().replaceFirst("i5.bml.transpiler.bot.", "");
+                            if (!outputPackage.isEmpty()) {
+                                i.setName("%s.%s".formatted(outputPackage, importName));
+                            } else {
+                                i.setName(importName);
+                            }
+                        });
+            });
+        }
 
-            // Copy build file template
-            var gradleTemplate = Files.readString(new File("transpiler/src/main/resources/build_template").toPath(), Charset.defaultCharset());
-            FileUtils.copyFile(new File("transpiler/src/main/resources/example_training_data.yml"),
-                    new File(outputDir + "/src/main/resources/example_training_data.yml"));
+        // Create "project" folders
+        //noinspection ResultOfMethodCallIgnored
+        new File(outputDir + "/src/main/resources/").mkdir();
+        //noinspection ResultOfMethodCallIgnored
+        new File(outputDir + "/src/test/java/").mkdir();
+        //noinspection ResultOfMethodCallIgnored
+        new File(outputDir + "/src/test/resources/").mkdir();
 
-            // Replace templates in build.gradle template
-            ST gradleFile = new ST(gradleTemplate);
-            gradleFile.add("groupId", outputPackage);
-            gradleFile.add("needsDot", outputPackage.isEmpty() ? "" : ".");
-            gradleFile.add("mainClass", "BotMain");
-            Files.write(new File(outputDir + "/build.gradle").toPath(), gradleFile.render().getBytes());
+        // Copy build file template
+        var gradleTemplate = Files.readString(new File("transpiler/src/main/resources/build_template").toPath(), Charset.defaultCharset());
+        FileUtils.copyFile(new File("transpiler/src/main/resources/example_training_data.yml"),
+                new File(outputDir + "/src/main/resources/example_training_data.yml"));
 
-            // Copy gitignore
-            FileUtils.copyFile(new File("transpiler/src/main/resources/gitignore_template"), new File(outputDir + "/.gitignore"));
+        // Replace templates in build.gradle template
+        ST gradleFile = new ST(gradleTemplate);
+        gradleFile.add("groupId", outputPackage);
+        gradleFile.add("needsDot", outputPackage.isEmpty() ? "" : ".");
+        gradleFile.add("mainClass", "BotMain");
+        Files.write(new File(outputDir + "/build.gradle").toPath(), gradleFile.render().getBytes());
 
-            // Copy SLF4J SimpleLogger config
-            FileUtils.copyFile(new File("transpiler/src/main/resources/simplelogger.properties"), new File(outputDir + "/src/main/resources/simplelogger.properties"));
+        // Copy gitignore
+        FileUtils.copyFile(new File("transpiler/src/main/resources/gitignore_template"), new File(outputDir + "/.gitignore"));
 
-            // Emit code into output directory
-            new JavaTreeGenerator(outputDir + "/src/main/java/", outputPackage).visit(tree);
+        // Copy SLF4J SimpleLogger config
+        FileUtils.copyFile(new File("transpiler/src/main/resources/simplelogger.properties"), new File(outputDir + "/src/main/resources/simplelogger.properties"));
 
-            if (outputFormat.equals("jar")) {
-                // TODO: Compile code, output jar, delete output files except jar
-            }
+        // Emit code into output directory
+        Measurements.measure("Code generation", () -> new JavaTreeGenerator(outputDir + "/src/main/java/", outputPackage).visit(tree));
+
+        if (outputFormat.equals("jar")) {
+            outputJar();
+        }
+    }
+
+    private void outputJar() {
+        LOGGER.info("Starting compilation process ...");
+
+        ProjectConnection connection = GradleConnector.newConnector()
+                .useGradleVersion("7.5.1")
+                .forProjectDirectory(new File(outputDir))
+                .connect();
+
+        try (connection) {
+            connection.newBuild()
+                    .forTasks("jar")
+                    .setStandardOutput(System.out)
+                    .run(new ResultHandler<>() {
+                        @Override
+                        public void onComplete(Void result) {
+                            LOGGER.info("Compilation process done");
+                        }
+
+                        @Override
+                        public void onFailure(GradleConnectionException failure) {
+                            LOGGER.info("Compilation process failed:\n{}", failure.toString());
+                        }
+                    });
+        } catch (IllegalStateException e) {
+            LOGGER.error("Compilation process failed", e);
         }
     }
 
