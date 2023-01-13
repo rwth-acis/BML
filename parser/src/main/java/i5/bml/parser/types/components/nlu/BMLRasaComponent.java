ff6933aaa9bb53cd23f5287998822a9aad5fdff4
Marc Luqué <marc.luque@outlook.com>
2023-01-13 16:04:57 +0100

    Parser: Rasa component is a non-complex type, hence, we do not need an encoded string

--- parser/src/main/java/i5/bml/parser/types/components/nlu/BMLRasaComponent.java
+++ parser/src/main/java/i5/bml/parser/types/components/nlu/BMLRasaComponent.java
@@ -8,11 +8,6 @@ public class BMLRasaComponent extends AbstractBMLType implements CanPopulatePara
     @BMLComponentParameter(name = "url", expectedBMLType = BuiltinType.STRING, isRequired = true)
     private String url;
 
-    @Override
-    public String encodeToString() {
-        return "%s{url='%s'}".formatted(getName(), url);
-    }
-
     public String getUrl() {
         return url;
     }
