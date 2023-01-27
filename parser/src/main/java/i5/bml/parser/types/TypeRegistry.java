0dcc2dd3004e41e16ba2a4d65943c89d9ccb218c
Marc Luqué <marc.luque@outlook.com>
2023-01-27 16:31:44 +0100

    Parser: Add documentation for type registry

--- parser/src/main/java/i5/bml/parser/types/TypeRegistry.java
+++ parser/src/main/java/i5/bml/parser/types/TypeRegistry.java
@@ -26,6 +26,22 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * TypeRegistry is a utility class that provides methods for registering, resolving, and querying types.
+ * Types can be registered by calling {@link TypeRegistry#registerType(Type)}, and resolved by calling
+ * {@link TypeRegistry#resolveType(String)}, {@link TypeRegistry#resolveType(Type)},
+ * {@link TypeRegistry#resolveType(BuiltinType)}, or {@link TypeRegistry#resolveComplexType(String)}.
+ * Additionally, it provides methods for querying whether a type is builtin or complex and for getting
+ * built-in annotations.
+ * <p>
+ * The class plays a central role in the re-usability of types. The underlying idea is that types are singletons, i.e.,
+ * there can only exist one type. This is quite obvious in the case of Strings or Booleans. However, for a component
+ * of type {@link BMLOpenAPIComponent} it is different, since the type is determined by the specific OpenAPI specification.
+ * The class {@link AbstractBMLType} provides means for types to define what makes them equal. In the case of Strings and
+ * Booleans this could simply be class equality, i.e., the `instanceof` relation. In the case of an OpenAPI component
+ * it should be class equality and specification equality (e.g., same url).
+ */
+
 public class TypeRegistry {
 
     private static final Map<String, Type> registeredTypes = new HashMap<>();
@@ -38,7 +54,8 @@ public class TypeRegistry {
 
     private static int typeIndex = 0;
 
-    private TypeRegistry() {}
+    private TypeRegistry() {
+    }
 
     static {
         init();
@@ -60,6 +77,13 @@ public class TypeRegistry {
         return resolveComplexType(typeName.name());
     }
 
+    /**
+     * Resolves a complex type (see {@link BMLType#isComplex()}) by its name.
+     *
+     * @param typeName The name of the complex type to resolve.
+     * @return An instance of the complex type, or null if the type is not registered.
+     * @throws IllegalStateException if the class does not have a default constructor or if the class does not extend AbstractBMLType.
+     */
     public static Type resolveComplexType(String typeName) {
         var clazz = complexTypeBlueprints.get(typeName.toLowerCase());
         if (clazz == null) {
@@ -93,20 +117,15 @@ public class TypeRegistry {
         return complexTypeBlueprints.containsKey(typeName.toLowerCase());
     }
 
+    /**
+     * Registers a type class as a complex or primitive type.
+     *
+     * @param typeClass the class of the type to register.
+     * @throws IllegalStateException if the class does not have an empty default constructor, or
+     *                               if the class does not have the annotation BMLType, or if the class does not extend AbstractBMLType.
+     */
     private static void registerType(Class<?> typeClass) {
         BMLType type = typeClass.getAnnotation(BMLType.class);
-
-        // Check: class extends AbstractBMLType
-//        if (!AbstractBMLType.class.isAssignableFrom(typeClass)) {
-//            throw new IllegalStateException("Class %s does not does not extend %s".formatted(typeClass.getName(), AbstractBMLType.class.getName()));
-//        }
-
-        // Check: class has default constructor with no parameters
-//        boolean hasDefaultConstructor = Arrays.stream(typeClass.getDeclaredConstructors())
-//                .anyMatch(constructor -> constructor.getParameterCount() == 0);
-//        if (!hasDefaultConstructor) {
-//            throw new IllegalStateException("Class %s does not have an empty default constructor".formatted(typeClass.getName()));
-//        }
 
         if (type.isComplex()) {
             complexTypeBlueprints.put(type.name().toString().toLowerCase(), typeClass);
@@ -124,6 +143,10 @@ public class TypeRegistry {
         }
     }
 
+    /**
+     * Initializes the {@link TypeRegistry} by registering built-in types, annotations and types that are defined
+     * in the classpath. The classpath here refers to the subpackages of {@link i5.bml.parser.types}.
+     */
     public static void init() {
         for (var value : BuiltinType.values()) {
             if (!value.isInternal()) {
