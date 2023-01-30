430e09fbb6e7bf2a7b006c25e55acbcdd3c64677
Marc Luqué <marc.luque@outlook.com>
2023-01-30 13:55:00 +0100

    Parser: Documentation for BlockScope

--- parser/src/main/java/i5/bml/parser/symbols/BlockScope.java
+++ parser/src/main/java/i5/bml/parser/symbols/BlockScope.java
@@ -3,14 +3,29 @@ package i5.bml.parser.symbols;
 import org.antlr.symtab.BaseScope;
 import org.antlr.symtab.Scope;
 
+/**
+ * Represents a block scope in a BML program.
+ * Also see {@link BaseScope}.
+ */
 public class BlockScope extends BaseScope {
 
+    /**
+     * Creates a new instance of block scope with given enclosing scope.
+     *
+     * @param enclosingScope The enclosing scope of this block scope.
+     */
     public BlockScope(Scope enclosingScope) {
         super(enclosingScope);
     }
 
+    /**
+     * Returns the name of this scope, which is "local".
+     *
+     * @return the name of the scope.
+     */
     @Override
     public String getName() {
         return "local";
     }
 }
+
