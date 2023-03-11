72456cdf9a577b304e383ac8e9586f919ae064c2
Marc Luqué <marc.luque@outlook.com>
2023-03-11 17:52:34 +0100

    Parser: Make bot arguments optional

--- parser/src/main/java/i5/bml/parser/types/BMLBot.java
+++ parser/src/main/java/i5/bml/parser/types/BMLBot.java
@@ -3,10 +3,10 @@ package i5.bml.parser.types;
 @BMLType(name = BuiltinType.BOT, isComplex = false)
 public class BMLBot extends AbstractBMLType implements CanPopulateParameters {
 
-    @BMLComponentParameter(name = "host", expectedBMLType = BuiltinType.STRING, isRequired = true)
+    @BMLComponentParameter(name = "host", expectedBMLType = BuiltinType.STRING, isRequired = false)
     private String host;
 
-    @BMLComponentParameter(name = "port", expectedBMLType = BuiltinType.NUMBER, isRequired = true)
+    @BMLComponentParameter(name = "port", expectedBMLType = BuiltinType.NUMBER, isRequired = false)
     private String port;
 
     @BMLComponentParameter(name = "name", expectedBMLType = BuiltinType.STRING, isRequired = false)
